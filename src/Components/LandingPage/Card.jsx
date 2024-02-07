9dd07d0d9d6dd06f0adb5c2de52f4fcbc21edbfd
Richard Jeremy Githuba <githuba9520@gmail.com>
2024-02-07 09:53:32 +0300

    Added the scaffolding for the cards and worked on the page formating

--- src/Components/LandingPage/Card.jsx
+++ src/Components/LandingPage/Card.jsx
@@ -1,8 +1,21 @@
-
 function Card() {
   return (
-    <div>Card</div>
-  )
+    <div className="card-container">
+      <div className="card-heading">
+        <h2 className="header-text">Starter</h2>
+        <h1 className="large-text">$19</h1>
+      </div>
+      <ul className="normal-text accent-bullets ">
+        <li>500 MAUs</li>
+        <li>3 projects</li>
+        <li>Unlimited guides</li>
+        <li>Unimited flows</li>
+        <li>Unlimited branded themes</li>
+        <li>Email support</li>
+      </ul>
+      <button className="medium-text">Choose Plan</button>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
--- src/Components/LandingPage/PriceSection.jsx
+++ src/Components/LandingPage/PriceSection.jsx
@@ -2,7 +2,7 @@ import Card from "./Card"
 function PriceSection() {
   return (
     <div className="price-section-container">
-      <h2 className="header-text" >Choose a plan</h2>
+      <h2 className="title-text" >Choose a plan</h2>
       <p className="normal-text">Billed monthly Billed annually </p>
       <div className="cards">
         <Card />
