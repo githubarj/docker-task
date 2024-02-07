f29d48ee36e49b90ec9af8f7b37d6f9801eca7e3
Richard Jeremy Githuba <githuba9520@gmail.com>
2024-02-07 13:29:37 +0300

    created a component for the dropdown

---
+++ src/Components/DropDown/DropDown.jsx
@@ -0,0 +1,25 @@
+
+import { useState } from "react";
+
+function Dropdown() {
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <div>
+      <select value={selectedOption} onChange={handleSelectChange}>
+        <option value="">Select an option</option>
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+      </select>
+
+      <p>Selected Option: {selectedOption}</p>
+    </div>
+  );
+}
+
+export default Dropdown;
