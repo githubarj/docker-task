5bdea412f473887067196a331541f392387234bf
Richard Jeremy Githuba <githuba9520@gmail.com>
2024-02-07 10:51:41 +0300

    created a data folder and added adata file to it containing prices and features

---
+++ src/Data/Prices.js
@@ -0,0 +1,37 @@
+export const prices = [
+  {
+    product: "Starter",
+    monthly: "$19",
+    annually: "$228",
+    features: [
+      "500 MAUs",
+      "3 projects",
+      "Unlimited guides",
+      "Unimited flows",
+      "Unlimited branded themes",
+      "Email support",
+    ],
+  },
+  {
+    product: "Pro",
+    monthly: "$99",
+    annually: "$1188",
+    features: [
+      "All starter features, plus:",
+      "Unlimited projects",
+      "Unlimited fully customizable themes",
+      "A dedicated Customer Success manager",
+    ],
+  },
+  {
+    product: "Enterprise",
+    price: "Let's Talk!",
+    features: [
+      "All pro features",
+      "Unlimited MAUs",
+      "Dedicated environment",
+      "Enterprise account administration",
+      "Premium support and services",
+    ],
+  },
+];
